f3425a1ba4f61f6e4727480786a04c206bafa9e8
geraldojalves <junior.alves.2007@gmail.com>
2024-02-11 20:36:52 -0300

    feat: add Product entity

--- src/modules/@shared/domain/entity/base.entity.ts
+++ src/modules/@shared/domain/entity/base.entity.ts
@@ -1,11 +1,11 @@
-import { Id } from "@/@shared/domain/value-object";
+import { Id } from "@/modules/@shared/domain/value-object";
 
 export default class BaseEntity {
-    private id: Id;
-    private createdAt: Date;
-    private updatedAt: Date;
+    protected id: Id;
+    protected createdAt: Date;
+    protected updatedAt: Date;
 
-    constructor(id: Id, createdAt: Date, updatedAt: Date) {
+    constructor(id?: Id, createdAt?: Date, updatedAt?: Date) {
         this.id = id || new Id();
         this.createdAt = createdAt || new Date();
         this.updatedAt = updatedAt || new Date();
---
+++ src/modules/product-adm/domain/product.entity.ts
@@ -0,0 +1,44 @@
+import { AggregateRoot, BaseEntity } from "@/modules/@shared/domain/entity";
+import { Id } from "@/modules/@shared/domain/value-object";
+
+type ProductProps = {
+    id?: Id;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    stock: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export default class Product extends BaseEntity implements AggregateRoot {
+
+    private name;
+    private description;
+    private purchasePrice;
+    private stock;
+
+    constructor(props: ProductProps) {
+        super(props.id, props.createdAt, props.updatedAt)
+        this.name = props.name
+        this.description = props.description;
+        this.purchasePrice = props.purchasePrice;
+        this.stock = props.stock;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getPurchasePrice() {
+        return this.purchasePrice;
+    }
+
+    getStock() {
+        return this.stock;
+    }
+}
