e7013cab6f6dbf3cc9c1c301bb3a1bd6c2273cf7
geraldojalves <junior.alves.2007@gmail.com>
2024-02-14 19:43:04 -0300

    test: add payment facade

---
+++ src/modules/payment/facacde/payment.facade.spec.ts
@@ -0,0 +1,60 @@
+import { StatusTypes } from "../domain";
+import { TransactionRepository } from "../repository";
+import { ProcessPaymentUsecase } from "../usecase/process-payment";
+import PaymentFacade from "./payment.facade";
+
+const makeSut = () => {
+  const repository = new TransactionRepository();
+  const processPaymentUsecase = new ProcessPaymentUsecase(repository);
+
+  const sut = new PaymentFacade(processPaymentUsecase);
+
+  return {
+    sut,
+    processPaymentUsecase,
+  };
+};
+
+describe("Payment facade", () => {
+  describe("process()", () => {
+    it("should call ProcessPaymentUsecase", async () => {
+      const { sut, processPaymentUsecase } = makeSut();
+
+      const transaction = {
+        transactionId: "1",
+        orderId: "011",
+        amount: 190,
+        status: StatusTypes.APPROVED,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const processPaymentUsecaseSpy = jest
+        .spyOn(processPaymentUsecase, "execute")
+        .mockResolvedValueOnce(transaction);
+
+      const response = await sut.process({
+        orderId: transaction.orderId,
+        amount: transaction.amount,
+      });
+
+      expect(processPaymentUsecaseSpy).toHaveBeenCalled();
+      expect(response).toEqual(transaction);
+    });
+
+    it("should throw ProcessPaymentUsecase if throws", async () => {
+      const { sut, processPaymentUsecase } = makeSut();
+
+      jest
+        .spyOn(processPaymentUsecase, "execute")
+        .mockRejectedValueOnce(new Error("error model"));
+
+      await expect(async () => {
+        await sut.process({
+          orderId: "011",
+          amount: 190,
+        });
+      }).rejects.toThrow("error model");
+    });
+  });
+});
