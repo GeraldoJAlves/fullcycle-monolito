2420517273abd0ebd0727362cfca3c023b9292dc
geraldojalves <junior.alves.2007@gmail.com>
2024-02-14 12:08:10 -0300

    test: add transaction repository

---
+++ src/modules/payment/repository/transaction.repository.spec.ts
@@ -0,0 +1,56 @@
+import { Sequelize } from "sequelize-typescript";
+import TransactionModel from "./transaction.model";
+import TransactionRepository from "./transaction.repository";
+import { Transaction } from "../domain";
+
+describe("Transaction repository", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      database: ":memory:",
+      logging: false,
+      sync: {
+        force: true,
+      },
+    });
+
+    await sequelize.addModels([TransactionModel])
+    await sequelize.sync()
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  describe("save()", () => {
+    it("should save a transaction", async () => {
+      const transaction = new Transaction({
+        orderId: "o-11",
+        amount: 20,
+      });
+
+      const repository = new TransactionRepository();
+
+      const transactionResult = await repository.save(transaction);
+
+      const transactionDb = await TransactionModel.findOne({
+        where: {
+          transactionId: transaction.getId().getValue(),
+        },
+      });
+
+      expect(transactionDb).toBeDefined();
+      expect(transactionDb?.transactionId).toBe(transaction.getId().getValue());
+      expect(transactionDb?.orderId).toBe(transaction.getOrderId());
+      expect(transactionDb?.amount).toBe(transaction.getAmount());
+      expect(transactionDb?.status).toBe(transaction.getStatus());
+      expect(transactionDb?.createdAt).toEqual(transaction.getCreatedAt());
+      expect(transactionDb?.updatedAt).toEqual(transaction.getUpdatedAt());
+      expect(transactionResult).toEqual(transaction);
+
+      await TransactionModel.findOne();
+    });
+  });
+});
