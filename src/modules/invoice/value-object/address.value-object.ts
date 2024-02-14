87f038b0c515664fff2f9f5bb0eeaa6bb568882a
geraldojalves <junior.alves.2007@gmail.com>
2024-02-14 20:17:20 -0300

    feat: add address value object

---
+++ src/modules/invoice/value-object/address.value-object.ts
@@ -0,0 +1,36 @@
+import { ValueObject } from "@/modules/@shared/domain/value-object";
+
+export default class Address implements ValueObject {
+  constructor(
+    private street: string,
+    private number: string,
+    private complement: string,
+    private city: string,
+    private state: string,
+    private zipCode: string
+  ) {}
+
+  getStreet() {
+    return this.street;
+  }
+
+  getNumber() {
+    return this.number;
+  }
+
+  getComplement() {
+    return this.complement;
+  }
+
+  getCity() {
+    return this.city;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getZipCode() {
+    return this.zipCode;
+  }
+}
---
+++ src/modules/invoice/value-object/index.ts
@@ -0,0 +1,3 @@
+import Address from "./address.value-object";
+
+export { Address };
