4fa9df376ed6914cc49c735b670a5f30d5f065db
geraldojalves <junior.alves.2007@gmail.com>
2024-02-14 22:02:21 -0300

    test: add invoice factory

---
+++ src/modules/invoice/factory/entity.factory.spec.ts
@@ -0,0 +1,45 @@
+import { Id } from "@/modules/@shared/domain/value-object";
+import InvoiceFactory from "./entity.factory";
+import { Address } from "../value-object";
+
+describe("Invoice factory", () => {
+  it("should create an invoice", () => {
+    const input = {
+      name: "July",
+      document: "doc 1",
+      street: "street 1",
+      number: "40",
+      complement: "house",
+      city: "city B",
+      state: "AM",
+      zipCode: "9999-99",
+      items: [
+        {
+          name: "ball",
+          price: 10,
+        },
+        {
+          name: "table",
+          price: 30,
+        },
+      ],
+    };
+
+    const invoice = InvoiceFactory.create(input);
+
+    expect(invoice.getId()).toBeInstanceOf(Id);
+    expect(invoice.getName()).toBe(input.name)
+    expect(invoice.getDocument()).toBe(input.document)
+    expect(invoice.getAddress()).toBeInstanceOf(Address)
+    expect(invoice.getAddress().getStreet()).toBe(input.street)
+    expect(invoice.getAddress().getNumber()).toBe(input.number)
+    expect(invoice.getAddress().getComplement()).toBe(input.complement)
+    expect(invoice.getAddress().getCity()).toBe(input.city)
+    expect(invoice.getAddress().getState()).toBe(input.state)
+    expect(invoice.getAddress().getZipCode()).toBe(input.zipCode)
+    expect(invoice.getItems().length).toBe(input.items.length)
+    expect(invoice.getItems()[0].getName()).toBe(input.items[0].name)
+    expect(invoice.getItems()[1].getName()).toBe(input.items[1].name)
+    expect(invoice.getTotal()).toBe(40)
+  });
+});
