12748ffff454a5fb3389fd3c3db40b268729238a
geraldojalves <junior.alves.2007@gmail.com>
2024-02-15 22:32:44 -0300

    feat: add generate invoice facade

--- src/modules/invoice/facade/invoice.facade.ts
+++ src/modules/invoice/facade/invoice.facade.ts
@@ -1,11 +1,22 @@
-import { FindInvoiceUsecase } from "../usecase/find-invoice";
+import { FindInvoiceUsecase } from "@/modules/invoice/usecase/find-invoice";
+import { GenerateInvoiceUsecase } from "@/modules/invoice/usecase/generate-invoice";
 import InvoiceFacadeInterface, {
   FindInvoiceFacadeInputDTO,
   FindInvoiceFacadeOutputDTO,
+  GenerateInvoiceFacadeInputDTO,
+  GenerateInvoiceFacadeOutputDTO,
 } from "./invoice.facade.interface";
 
 export default class InvoiceFacade implements InvoiceFacadeInterface {
-  constructor(private readonly findInvoiceUsecase: FindInvoiceUsecase) {}
+  constructor(
+    private readonly findInvoiceUsecase: FindInvoiceUsecase,
+    private readonly generateInvoiceUsecase: GenerateInvoiceUsecase
+  ) {}
+  generate(
+    input: GenerateInvoiceFacadeInputDTO
+  ): Promise<GenerateInvoiceFacadeOutputDTO> {
+    return this.generateInvoiceUsecase.execute(input);
+  }
 
   find(input: FindInvoiceFacadeInputDTO): Promise<FindInvoiceFacadeOutputDTO> {
     return this.findInvoiceUsecase.execute(input);
