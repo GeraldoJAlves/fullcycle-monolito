7efd0c3a8775d55f7e43d2e8565f66de2a6272ec
geraldojalves <junior.alves.2007@gmail.com>
2024-02-17 22:48:06 -0300

    test: approve an order for checkout

--- src/modules/checkout/domain/order.spec.ts
+++ src/modules/checkout/domain/order.spec.ts
@@ -13,6 +13,7 @@ describe("Order entity", () => {
 
     const products = [
       new Product({ name: "ball", description: "blue", salesPrice: 7.66 }),
+      new Product({ name: "ball", description: "red", salesPrice: 9.66 }),
     ];
 
     const input = {
@@ -26,5 +27,47 @@ describe("Order entity", () => {
     expect(order.getClient()).toEqual(client);
     expect(order.getProducts()).toEqual(products);
     expect(order.getStatus()).toEqual("pending");
+    expect(order.getTotal()).toEqual(17.32);
+  });
+
+  it("should aprove an order", () => {
+    const client = new Client({
+      name: "John",
+      email: "email@email.com",
+      address: "street 1, California",
+    });
+
+    const products = [
+      new Product({ name: "ball", description: "blue", salesPrice: 7.66 }),
+      new Product({ name: "ball", description: "red", salesPrice: 9.66 }),
+    ];
+
+    const input = {
+      client,
+      products,
+    };
+
+    const order = new Order(input);
+
+    order.approve();
+
+    expect(order.getStatus()).toEqual("approved");
+  });
+
+  it("should throw when order is created without products", () => {
+    const client = new Client({
+      name: "John",
+      email: "email@email.com",
+      address: "street 1, California",
+    });
+
+    const input = {
+      client,
+      products: []
+    };
+
+    expect(() => {
+      new Order(input);
+    }).toThrow("Order must have at least one product");
   });
 });
