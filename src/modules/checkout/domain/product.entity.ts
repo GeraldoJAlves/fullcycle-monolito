a9870ba4b0534e54759c1d00628bb7a66d62f799
geraldojalves <junior.alves.2007@gmail.com>
2024-02-17 22:40:21 -0300

    feat: add product entity for checkout

---
+++ src/modules/checkout/domain/product.entity.ts
@@ -0,0 +1,34 @@
+import { AggregateRoot, BaseEntity } from "@/modules/@shared/domain/entity";
+import { Id } from "@/modules/@shared/domain/value-object";
+
+type ProductProps = {
+  id?: Id;
+  name: string;
+  description: string;
+  salesPrice: number;
+};
+
+export default class Product extends BaseEntity implements AggregateRoot {
+  private name: string;
+  private description: string;
+  private salesPrice: number;
+
+  constructor(props: ProductProps) {
+    super(props.id);
+    this.name = props.name;
+    this.description = props.description;
+    this.salesPrice = props.salesPrice;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  getSalesPrice() {
+    return this.salesPrice;
+  }
+}
