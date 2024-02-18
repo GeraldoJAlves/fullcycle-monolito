c19591866f42c7da5a16700c94c5741a4a81e3cc
geraldojalves <junior.alves.2007@gmail.com>
2024-02-17 22:40:28 -0300

    feat: add order entity for checkout

---
+++ src/modules/checkout/domain/order.entity.ts
@@ -0,0 +1,43 @@
+import { AggregateRoot, BaseEntity } from "@/modules/@shared/domain/entity";
+import { Id } from "@/modules/@shared/domain/value-object";
+import Client from "./client.entity";
+import Product from "./product.entity";
+
+type OrderProps = {
+  id?: Id;
+  client: Client;
+  products: Product[];
+  status?: string;
+};
+
+export default class Order extends BaseEntity implements AggregateRoot {
+  private client: Client;
+  private products: Product[];
+  private status: string;
+
+  constructor(props: OrderProps) {
+    super(props.id);
+    this.client = props.client;
+    this.products = props.products;
+    this.status = props.status || "pending";
+    this.validate()
+  }
+
+  private validate() {
+    if (this.products.length === 0) {
+      throw new Error("Order must have at least one product");
+    }
+  }
+
+  getClient() {
+    return this.client;
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getStatus() {
+    return this.status;
+  }
+}
