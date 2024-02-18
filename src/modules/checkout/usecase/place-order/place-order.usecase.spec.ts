f4b6d6c9e4c1337bceba92a05d885c9e62224c8e
geraldojalves <junior.alves.2007@gmail.com>
2024-02-18 20:30:49 -0300

    test: find products in add place order usecase

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -1,14 +1,42 @@
-import ClientAdmFacadeFactory from "@/modules/client-adm/factory/facade.factory";
+import { ClientAdmFacadeFactory } from "@/modules/client-adm/factory";
+import { StoreCatalogFacadeFactory } from "@/modules/store-catalog/factory";
 import PlaceOrderUsecase from "./place-order.usecase";
+import { ClientAdmFacade } from "@/modules/client-adm/facade";
+import { StoreCatalogFacade } from "@/modules/store-catalog/facade";
 
 const makeSut = () => {
   const clientAdmFacade = ClientAdmFacadeFactory.create();
+  const storeCatalogFacade = StoreCatalogFacadeFactory.create();
 
-  const sut = new PlaceOrderUsecase(clientAdmFacade);
+  const sut = new PlaceOrderUsecase(clientAdmFacade, storeCatalogFacade);
+
+  defaultMock(clientAdmFacade, storeCatalogFacade);
   return {
     sut,
     clientAdmFacade,
+    storeCatalogFacade,
   };
+};
+
+const defaultMock = (
+  clientAdmFacade: ClientAdmFacade,
+  storeCatalogFacade: StoreCatalogFacade
+) => {
+  jest.spyOn(clientAdmFacade, "find").mockResolvedValue({
+    id: "c2",
+    name: "john",
+    email: "email@email 1",
+    address: "street 1",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  jest.spyOn(storeCatalogFacade, "find").mockResolvedValue({
+    id: "p1",
+    name: "ball",
+    description: "red",
+    salesPrice: 10,
+  });
 };
 
 describe("PlaceOrder usecase", () => {
@@ -22,8 +50,9 @@ describe("PlaceOrder usecase", () => {
 
       const clientAdmFacadeSpy = jest
         .spyOn(clientAdmFacade, "find")
+        .mockClear()
         .mockResolvedValueOnce({
-          id: "c1",
+          id: "c2",
           name: "john",
           email: "email@email 1",
           address: "street 1",
@@ -32,7 +61,7 @@ describe("PlaceOrder usecase", () => {
         });
 
       const input = {
-        clientId: "",
+        clientId: "c1",
         products: [{ productId: "1" }, { productId: "2" }],
       };
 
@@ -56,6 +85,55 @@ describe("PlaceOrder usecase", () => {
       await expect(async () => {
         await sut.execute(input);
       }).rejects.toThrow(new Error("Client not found"));
+    });
+  });
+
+  describe("storeCatalogFacade", () => {
+    it("should call StoreCatalogFacade", async () => {
+      const { sut, storeCatalogFacade } = makeSut();
+
+      const storeCatalogFacadeSpy = jest
+        .spyOn(storeCatalogFacade, "find")
+        .mockResolvedValueOnce({
+          id: "p1",
+          name: "ball",
+          description: "red",
+          salesPrice: 10,
+        });
+
+      const input = {
+        clientId: "c1",
+        products: [{ productId: "1" }, { productId: "2" }],
+      };
+
+      await sut.execute(input);
+
+      expect(storeCatalogFacadeSpy).toHaveBeenCalledTimes(
+        input.products.length
+      );
+    });
+
+    it("should throw when an empty list of products is provided", async () => {
+      const { sut, storeCatalogFacade } = makeSut();
+
+      const storeCatalogFacadeSpy = jest
+        .spyOn(storeCatalogFacade, "find")
+        .mockResolvedValueOnce({
+          id: "p1",
+          name: "ball",
+          description: "red",
+          salesPrice: 10,
+        });
+
+      const input = {
+        clientId: "",
+        products: [],
+      };
+
+      await expect(async () => {
+        await sut.execute(input);
+      }).rejects.toThrow(new Error("Must provide at least one product"));
+      expect(storeCatalogFacadeSpy).toHaveBeenCalledTimes(0);
     });
   });
 });
