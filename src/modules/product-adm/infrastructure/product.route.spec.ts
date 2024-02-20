76c2e71c0f975723c70c7b8c72ab995e709d2683
geraldojalves <junior.alves.2007@gmail.com>
2024-02-19 22:03:29 -0300

    test: validate body of POST /products

--- src/modules/product-adm/infrastructure/product.route.spec.ts
+++ src/modules/product-adm/infrastructure/product.route.spec.ts
@@ -12,12 +12,11 @@ describe("Product route", () => {
 
   describe("POST /", () => {
     it("should return 201 if a valid product is provided", async () => {
-
       const response = await request(app).post("/product").send({
         name: "ball",
         description: "red",
         purchasePrice: 2.99,
-        stock: 10
+        stock: 10,
       });
 
       expect(response.status).toBe(201);
@@ -26,6 +25,25 @@ describe("Product route", () => {
       expect(response.body.description).toBe("red");
       expect(response.body.stock).toBe(10);
       expect(response.body.purchasePrice).toBe(2.99);
+    });
+
+    it("should return 400 if an invalid body is provided", async () => {
+      const inputs = [
+        {},
+        { name: "ball" },
+        { description: "ball" },
+        { stock: 2 },
+        { purchasePrice: 3 },
+        { name: "ball", description: "red", purchasePrice: 0, stock: 10 },
+        { name: "ball", description: "red", purchasePrice: 2, stock: 0 },
+        { name: "ball", description: "red", purchasePrice: 2, stock: -1 },
+        { name: "ball", description: "red", purchasePrice: -1, stock: 10 },
+      ];
+
+      for (const input of inputs) {
+        const response = await request(app).post("/product").send(input);
+        expect(response.status).toBe(400);
+      }
     });
   });
 });
